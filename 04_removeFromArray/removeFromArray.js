49c57bc7cb657c22eb9e02df7f1281264e9ff607
Ariel Corcia <107890906+arielcorcia@users.noreply.github.com>
2024-03-31 22:43:23 -0400

    Complete js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...filter) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        for (let j = filter.length - 1; j >= 0; j--)
+            if (array[i] === filter[j]){
+                array.splice(i, 1)
+            }
+    }
+    return array
 };
 
 // Do not edit below this line
