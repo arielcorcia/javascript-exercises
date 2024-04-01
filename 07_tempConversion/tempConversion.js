8fc3006a79f285c41aada0dc45484c612c089ad4
Ariel Corcia <107890906+arielcorcia@users.noreply.github.com>
2024-04-01 12:40:24 -0400

    Complete js exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheittemp) {
+  let celsiustemp = (fahrenheittemp - 32) / 1.8
+  if (celsiustemp % 1 !== 0) {
+    return parseFloat(celsiustemp.toFixed(1))
+  } else {
+    return celsiustemp;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiustemp) {
+  let fahrenheittemp = (celsiustemp * 1.8) + 32;
+  if (fahrenheittemp % 1 !== 0 ){
+    return parseFloat(fahrenheittemp.toFixed(1))
+  } else {
+    return fahrenheittemp
+  }
 };
 
 // Do not edit below this line
