76aae1ab3e07808f15122ad3e6978a62c9280c2e
Ariel Corcia <107890906+arielcorcia@users.noreply.github.com>
2024-04-01 11:45:34 -0400

    Complete js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let buffer = 0;
 
+    if (num1 < 0 || num2 < 0 && typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR"
+    }
+
+    if (num1 > num2){
+        buffer = num1;
+        num1 = num2;
+        num2 = buffer;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    return sum;
 };
 
+let x = sumAll(10,"90");
 // Do not edit below this line
 module.exports = sumAll;
